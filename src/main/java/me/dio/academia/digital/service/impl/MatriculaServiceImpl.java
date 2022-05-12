83171cee354229132e8f0c212a898650ed42c5f1
Itallo <italloalexsander@gmail.com>
2022-05-12 17:29:55 -0300

    Bean do MatriculaServiceImpl ajustado

--- src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -7,9 +7,11 @@ import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.repository.MatriculaRepository;
 import me.dio.academia.digital.service.IMatriculaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MatriculaServiceImpl implements IMatriculaService {
 
     @Autowired
